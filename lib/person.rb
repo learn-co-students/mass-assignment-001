c16312361b22d5b3e9aa909152aa8a6f7bbd6ac4
halfields <hfields697@aol.com>
2016-05-15 02:16:28 -0500

    done

--- lib/person.rb
+++ lib/person.rb
@@ -7,30 +7,3 @@ class Person
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
-
-  avi_attributes = {
-      :name => "Avi",
-      :birthday => "01/29/1984",
-      :hair_color => "brown",
-      :eye_color => "brown",
-      :height => "short",
-      :weight => "good",
-      :handed => "lefty",
-      :complexion => "decent",
-      :t_shirt_size => "medium",
-      :wrist_size => "small",
-      :glove_size => "normal",
-      :pant_length => "32",
-      :pant_width => "32"
-    }
-
-  spencer_attributes = {
-      :name => "Spencer",
-      :hair_color => "N/A",
-      :height => "medium",
-      :weight => "good",
-      :handed => "righty",
-    }
-
-  avi = Person.new(avi_attributes)
-  spencer = Person.new(spencer_attributes)
